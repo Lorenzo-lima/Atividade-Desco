db6224bfc9f9cb1431a9ea5403a474e1f82314ff
Lorenzo Lima <123124446+Lorenzo-lima@users.noreply.github.com>
2024-05-27 19:18:33 -0300

    padrao

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,12 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
  
 function Header() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="secondary">
+        <AppBar position="static">
           <Toolbar>
             <IconButton
               size="large"
@@ -20,7 +20,7 @@ function Header() {
               sx={{ mr: 2 }}
             >
               {
-              <MenuIcon />
+              
               }
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
